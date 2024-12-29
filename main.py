0c3e9a40ae78985daa427ad66cb87d4424537e49
mmiki057 <147617641+mmiki057@users.noreply.github.com>
2024-12-29 21:01:57 +0300

    Пофиксил баги

--- main.py
+++ main.py
@@ -9,18 +9,9 @@ bot = telebot.TeleBot('8053455390:AAGVSy0-_GGX4yaF0J9yHcB8xXM94jBBh3A')
 # Словарь для отслеживания состояний пользователей
 user_states = {}
 
-# Команда /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    user_id = message.from_user.id
-    username = message.from_user.username or "unknown"
-    user_profile = get_user_profile(user_id)  # Возвращает данные профиля или None
-    if not user_profile:
-        register_message = register_user(user_id, username)  # Регистрация пользователя
-        print(register_message)
-
+def get_main_menu():
     keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton('📋 Список карт', callback_data='list_items'))
+    keyboard.add(InlineKeyboardButton('📋 Список карт / Купить', callback_data='list_items'))
     keyboard.row(
         InlineKeyboardButton('‼️ Правила ‼️', callback_data='rules'),
         InlineKeyboardButton('📣 Поддержка', callback_data='support')
@@ -31,11 +22,22 @@ def start(message):
     )
     keyboard.add(InlineKeyboardButton('🔄 Возврат товара', callback_data='refund'))
     keyboard.add(InlineKeyboardButton('🛠️ Панель поставщика', callback_data='supplier_panel'))
+    return keyboard
+
+# Команда /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_id = message.from_user.id
+    username = message.from_user.username or "unknown"
+    user_profile = get_user_profile(user_id)  # Возвращает данные профиля или None
+    if not user_profile:
+        register_message = register_user(user_id, username)  # Регистрация пользователя
+        print(register_message)
 
     bot.send_message(
         message.chat.id,
         text='Добро пожаловать в маркет! В маркете представлены качественные материалы по доступным ценам. \n\nСовершая покупку, вы подтверждаете, что ознакомлены с правилами покупки и возврата материала.',
-        reply_markup=keyboard
+        reply_markup=get_main_menu()
     )
 
 # Обработка нажатий на кнопки
@@ -43,119 +45,129 @@ def start(message):
 def handle_callback(call: CallbackQuery):
     data = call.data
 
-    if data == 'list_items':
-        keyboard = InlineKeyboardMarkup()
-        keyboard.add(InlineKeyboardButton('Бины', callback_data='bins'))
-        keyboard.add(InlineKeyboardButton('🌍 Гео', callback_data='geo'))
-        keyboard.add(InlineKeyboardButton('🔍 Поиск по BIN', callback_data='search_bin'))
-        keyboard.add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
+    try:
+        if data == 'list_items':
+            keyboard = InlineKeyboardMarkup()
+            keyboard.add(InlineKeyboardButton('Бины', callback_data='bins'))
+            keyboard.add(InlineKeyboardButton('🌍 Гео', callback_data='geo'))
+            keyboard.add(InlineKeyboardButton('🔍 Поиск по BIN', callback_data='search_bin'))
+            keyboard.add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
 
-        bot.edit_message_text(
-            text='📋 Выберите категорию:',
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=keyboard
-        )
+            bot.edit_message_text(
+                text='📋 Выберите категорию:',
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=keyboard
+            )
 
-    elif data == 'bins':
-        bins = get_unique_bins()
-        keyboard = InlineKeyboardMarkup()
-        for bin_code in bins:
-            keyboard.add(InlineKeyboardButton(bin_code, callback_data=f'bin_{bin_code}'))
-        keyboard.add(InlineKeyboardButton('🔙 Назад', callback_data='list_items'))
+        elif data == 'bins':
+            bins = get_unique_bins()
+            keyboard = InlineKeyboardMarkup()
+            for bin_code in bins:
+                keyboard.add(InlineKeyboardButton(bin_code, callback_data=f'bin_{bin_code}'))
+            keyboard.add(InlineKeyboardButton('🔙 Назад', callback_data='list_items'))
 
-        bot.edit_message_text(
-            text="Выберите BIN:",
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=keyboard
-        )
+            bot.edit_message_text(
+                text="Выберите BIN:",
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=keyboard
+            )
 
-    elif data == 'geo':
-        geo_data = get_unique_geos()
-        keyboard = InlineKeyboardMarkup()
-        for geo in geo_data:
-            keyboard.add(InlineKeyboardButton(geo, callback_data=f'geo_{geo}'))
-        keyboard.add(InlineKeyboardButton('🔙 Назад', callback_data='list_items'))
+        elif data == 'geo':
+            geo_data = get_unique_geos()
+            keyboard = InlineKeyboardMarkup()
+            for geo in geo_data:
+                keyboard.add(InlineKeyboardButton(geo, callback_data=f'geo_{geo}'))
+            keyboard.add(InlineKeyboardButton('🔙 Назад', callback_data='list_items'))
 
-        bot.edit_message_text(
-            text="🌍 Выберите геолокацию:",
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=keyboard
-        )
+            bot.edit_message_text(
+                text="🌍 Выберите геолокацию:",
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=keyboard
+            )
 
-    elif data.startswith('bin_'):
-        bin_prefix = data.split('_')[1]
-        results = search_by_bin(bin_prefix)
-        text = "Результаты поиска:\n"
-        for geo, bin_code in results:
-            text += f"🌍 {geo}, BIN: {bin_code}\n"
+        elif data.startswith('bin_'):
+            bin_prefix = data.split('_')[1]
+            results = search_by_bin(bin_prefix)
+            text = "Результаты поиска:\n"
+            for geo, bin_code in results:
+                text += f"🌍 {geo}, BIN: {bin_code}\n"
 
-        bot.edit_message_text(
-            text=text,
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='bins'))
-        )
+            bot.edit_message_text(
+                text=text,
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='bins'))
+            )
 
-    elif data.startswith('geo_'):
-        geo = data.split('_')[1]
-        results = search_by_geo(geo)
-        text = "Результаты поиска:\n"
-        for geo, bin_code in results:
-            text += f"🌍 {geo}, BIN: {bin_code}\n"
+        elif data.startswith('geo_'):
+            geo = data.split('_')[1]
+            results = search_by_geo(geo)
+            text = "Результаты поиска:\n"
+            for geo, bin_code in results:
+                text += f"🌍 {geo}, BIN: {bin_code}\n"
 
-        bot.edit_message_text(
-            text=text,
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='geo'))
-        )
+            bot.edit_message_text(
+                text=text,
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='geo'))
+            )
 
-    elif data == 'search_bin':
-        bot.edit_message_text(
-            text="Введите первые 7 или 8 цифр BIN для поиска:",
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='list_items'))
-        )
-        user_states[call.message.chat.id] = 'awaiting_bin_input'
+        elif data == 'search_bin':
+            bot.edit_message_text(
+                text="Введите первые 7 или 8 цифр BIN для поиска:",
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='list_items'))
+            )
+            user_states[call.message.chat.id] = 'awaiting_bin_input'
 
-    elif data == 'rules':
-        bot.edit_message_text(
-            text='Совершая покупку в нашем магазине, вы автоматически соглашаетесь с этими правилами:\n1. Передача товара 3-м лицам запрещена, в случае выяснения факта передачи, мы имеем право отказать вам в замене материала.\n2. Денежные средства возврату не подлежат (мы можем сделать возврат только на баланс нашего бота)\n3. За рассмотрение всех заявок, рекламы, сотрудничества отвечает: @bullyman4ik',
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
-        )
+        elif data == 'rules':
+            bot.edit_message_text(
+                text='Совершая покупку в нашем магазине, вы автоматически соглашаетесь с этими правилами:\n1. Передача товара 3-м лицам запрещена, в случае выяснения факта передачи, мы имеем право отказать вам в замене материала.\n2. Денежные средства возврату не подлежат (мы можем сделать возврат только на баланс нашего бота)\n3. За рассмотрение всех заявок, рекламы, сотрудничества отвечает: @bullyman4ik',
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
+            )
 
-    elif data == 'support':
-        bot.edit_message_text(
-            text='Служба поддержки на связи 24/7. Писать: @bullyman4ik',
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
-        )
+        elif data == 'support':
+            bot.edit_message_text(
+                text='Служба поддержки на связи 24/7. Писать: @bullyman4ik',
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
+            )
 
-    elif data == 'profile':
-        user_id = call.from_user.id
-        user = get_user_profile(user_id)
-        if user:
-            username, balance, registered_at = user
-            text = f"👤 Профиль:\nUsername: {username}\nБаланс: {balance}💰\nДата регистрации: {registered_at}"
-        else:
-            text = "Профиль не найден. Попробуйте перезапустить бота командой /start."
+        elif data == 'profile':
+            user_id = call.from_user.id
+            user = get_user_profile(user_id)
+            if user:
+                username, balance, registered_at = user
+                text = f"👤 Профиль:\nUsername: {username}\nБаланс: {balance}💰\nДата регистрации: {registered_at}"
+            else:
+                text = "Профиль не найден. Попробуйте перезапустить бота командой /start."
 
-        bot.edit_message_text(
-            text=text,
-            chat_id=call.message.chat.id,
-            message_id=call.message.message_id,
-            reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
-        )
+            bot.edit_message_text(
+                text=text,
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton('🔙 Назад', callback_data='back_to_main'))
+            )
 
-    elif data == 'back_to_main':
-        start(call.message)
+        elif data == 'back_to_main':
+            bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.message_id,
+                text='Добро пожаловать в маркет! В маркете представлены качественные материалы по доступным ценам. \n\nСовершая покупку, вы подтверждаете, что ознакомлены с правилами покупки и возврата материала.',
+                reply_markup=get_main_menu()
+            )
+
+    except Exception as e:
+        print(f"Ошибка при обработке {data}: {e}")
+
 
 # Обработка текстового ввода
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'awaiting_bin_input')
